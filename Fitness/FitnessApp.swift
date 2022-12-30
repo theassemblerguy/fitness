80fd3bf802a145fb28e9cfab5200679962ce6915
Peter Bachmaier <git@neverseen.de>
2022-12-30 14:15:54 +0100

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Peter Bachmaier on 30.12.22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
